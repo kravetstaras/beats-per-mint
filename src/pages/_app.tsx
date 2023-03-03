d7f69fa3b6ea288ab4d26128870f6bef21bd4b5c
Taras.Kravets <taraskravets95@gmail.com>
2023-03-03 16:11:00 +0200

    feat(fonts): added google-fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,22 @@
 import { Provider } from 'next-auth/client';
 import { ThemeProvider } from '@mui/material';
 import type { AppProps } from 'next/app';
-import { Inter, Josefin_Sans, Arimo } from 'next/font/google';
 import CssBaseline from '@mui/material/CssBaseline';
+
 import MainHeader from '@/components/Layout/MainHeader';
 
+import { arimo } from '@/styles/fonts';
 import theme from '@/styles/material-theme';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<Provider session={pageProps.session}>
 			<ThemeProvider theme={theme}>
-				<CssBaseline />
 				<MainHeader />
-				<Component {...pageProps} />
+				<main className={arimo.className}>
+					<CssBaseline />
+					<Component {...pageProps} />
+				</main>
 			</ThemeProvider>
 		</Provider>
 	);
---
+++ src/styles/fonts.ts
@@ -0,0 +1,16 @@
+import { Inter, Josefin_Sans, Arimo } from 'next/font/google';
+
+export const inter = Inter({
+	subsets: ['latin'],
+	weight: ['700'],
+});
+
+export const josefinSans = Josefin_Sans({
+	subsets: ['latin'],
+	weight: ['400'],
+});
+
+export const arimo = Arimo({
+	subsets: ['latin'],
+	weight: ['400', '700'],
+});
