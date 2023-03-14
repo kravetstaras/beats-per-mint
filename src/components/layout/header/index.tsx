ae56bb9d965f2a39cec8e187484f358f71a66468
Taras.Kravets <taraskravets95@gmail.com>
2023-03-14 11:05:29 +0200

    fix(HeaderMenu): fixed closing HeaderMenu after click on MenuItems

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -16,81 +16,93 @@ import { header, headerNavBar } from '@/styles/header';
 const heaaderNavContent = [{ title: 'discover', id: 1, link: '/' }];
 
 export default function Header() {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const [session, loading] = useSession();
-  const router = useRouter();
+	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+	const [session, loading] = useSession();
+	const router = useRouter();
 
-  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
+	const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+		setAnchorEl(event.currentTarget);
+	};
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
 
-  return (
-    <Box sx={header}>
-      <Link href='/'>
-        <Image
-          src='/assets/icons/logo.png'
-          alt='BPM logo'
-          width={80}
-          height={80}
-        />
-      </Link>
-      <Box component='nav' sx={headerNavBar}>
-        {heaaderNavContent.map((link) => (
-          <Link
-            key={link.id}
-            href={link.link}
-            style={{
-              fontWeight: router.pathname === link.link ? '700' : '400',
-            }}>
-            {capitalizeFirstLetter(link.title)}
-          </Link>
-        ))}
-        {!session && !loading && (
-          <Link
-            href='/auth'
-            style={{
-              fontWeight: router.pathname === '/auth' ? '700' : '400',
-            }}>
-            Log in
-          </Link>
-        )}
-        {session && (
-          <div>
-            <IconButton
-              size='large'
-              aria-label='account of current user'
-              aria-controls='menu-appbar'
-              aria-haspopup='true'
-              onClick={handleMenu}
-              color='inherit'>
-              <AccountCircle fontSize='inherit' />
-            </IconButton>
-            <Menu
-              id='menu-appbar'
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorEl)}
-              onClose={handleClose}>
-              <MenuItem onClick={() => router.push('/profile')}>
-                Profile
-              </MenuItem>
-              <MenuItem onClick={() => signOut()}>Log out</MenuItem>
-            </Menu>
-          </div>
-        )}
-      </Box>
-    </Box>
-  );
+	const handleClickProfile = (event: React.MouseEvent<HTMLElement>) => {
+		handleClose();
+		router.push('/profile');
+	};
+
+	const handleClickLogOut = (event: React.MouseEvent<HTMLElement>) => {
+		handleClose();
+		signOut();
+	};
+
+	return (
+		<Box sx={header}>
+			<Link href='/'>
+				<Image
+					src='/assets/icons/logo.png'
+					alt='BPM logo'
+					width={80}
+					height={80}
+				/>
+			</Link>
+			<Box component='nav' sx={headerNavBar}>
+				{heaaderNavContent.map((link) => (
+					<Link
+						key={link.id}
+						href={link.link}
+						style={{
+							fontWeight: router.pathname === link.link ? '700' : '400',
+						}}
+					>
+						{capitalizeFirstLetter(link.title)}
+					</Link>
+				))}
+				{!session && !loading && (
+					<Link
+						href='/auth'
+						style={{
+							fontWeight: router.pathname === '/auth' ? '700' : '400',
+						}}
+					>
+						Log in
+					</Link>
+				)}
+				{session && (
+					<div>
+						<IconButton
+							size='large'
+							aria-label='account of current user'
+							aria-controls='menu-appbar'
+							aria-haspopup='true'
+							onClick={handleMenu}
+							color='inherit'
+						>
+							<AccountCircle fontSize='inherit' />
+						</IconButton>
+						<Menu
+							id='menu-appbar'
+							anchorEl={anchorEl}
+							anchorOrigin={{
+								vertical: 'top',
+								horizontal: 'right',
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: 'top',
+								horizontal: 'right',
+							}}
+							open={Boolean(anchorEl)}
+							onClose={handleClose}
+						>
+							<MenuItem onClick={handleClickProfile}>Profile</MenuItem>
+							<MenuItem onClick={handleClickLogOut}>Log out</MenuItem>
+						</Menu>
+					</div>
+				)}
+			</Box>
+		</Box>
+	);
 }
